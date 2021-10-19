83b54bb1735daa9b669b4a9b0b212c620ff30d81
Nikita <30903256+NikitosGolubev@users.noreply.github.com>
2021-10-19 15:04:31 +0300

    Added craco to support alliases

---
+++ craco.config.js
@@ -0,0 +1,21 @@
+/* eslint-disable */
+const path = require('path');
+
+const resolvePath = p => path.resolve(__dirname, p)
+
+module.exports = {
+  webpack: {
+    alias: {
+      '@components': resolvePath('./src/components'),
+      '@assets': resolvePath('./src/assets'),
+      '@typings': resolvePath('./src/typings'),
+      '@utils': resolvePath('./src/utils'),
+      '@pages': resolvePath('./src/pages'),
+      '@state': resolvePath('./src/state'),
+      '@config': resolvePath('./src/config'),
+      '@logic': resolvePath('./src/logic'),
+      '@abis': resolvePath('./src/abis'),
+      '@api': resolvePath('./src/api'),
+    },
+  },
+}
